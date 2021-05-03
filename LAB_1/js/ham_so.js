85de64adc0631911c11fc4abf8374efcda3be2dc
nhieupvpd03910 <phamvannhieu1144@gmail.com>
2021-05-03 20:35:56 +0700

    add

---
+++ LAB_1/js/ham_so.js
@@ -0,0 +1,9 @@
+﻿// JavaScript source code
+function Kiem_tra(x) {
+    if (x % 2 == 0) {
+        alert(x + " là số chẵn");
+    }
+    else {
+        alert(x + " là số lẻ");
+    }
+}
---
+++ Lab_3_nhieupvpd03910/bai_4/js_bai4.js
@@ -0,0 +1,43 @@
+﻿// JavaScript source code
+var a = null,b = null;
+var o = null;
+function lam_lai() {
+    return a = null,b=null,c=null ;
+}
+function toan_tu(x) {
+    o = x;
+}
+function toan_hang(x) {
+    if (a == null) {
+        a = x;
+    }
+    else {
+        b = x;
+    }
+}
+var kq = null;
+function thuc_hien() {
+    switch (o) {
+        case '+':
+            var kq = a + b;
+            alert(a + " + " + b + " = " + kq);
+            break;
+        case '-':
+            var kq = a - b;
+            alert(a + "-" + b + "=" + kq);
+            break;
+        case 'x':
+            var kq = a * b;
+            alert(a + "x" + b + "=" + kq);
+            break;
+        case ':':
+            var kq = a / b;
+            alert(a + ":" + b + "=" + kq);
+            break;
+        default:
+            alert(o + "Không phải là toán tử nhé");
+    }
+    lam_lai();
+}
+ 
+
